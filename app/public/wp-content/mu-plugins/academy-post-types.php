d5772c85b8be3607e825d38fc1d4c57b2e879f97
AhmedRabea0302 <arabea169@gmail.com>
2023-08-01 23:01:19 +0300

    Registered Events Custom post type

---
+++ app/public/wp-content/mu-plugins/academy-post-types.php
@@ -0,0 +1,16 @@
+<?php
+
+    add_action('init', 'academy_post_types');
+    function academy_post_types() {
+        register_post_type('event', array(
+            'public' => true,
+            'menu_icon' => 'dashicons-calendar-alt',
+            'labels' => array(
+                'name' => 'Events',
+                'add_new_item' => 'Add New Event',
+                'edit_item' => 'Edit Event',
+                'all_items' => 'All Events',
+                'singular_name' => 'Event'
+            ),
+        ));
+    }
