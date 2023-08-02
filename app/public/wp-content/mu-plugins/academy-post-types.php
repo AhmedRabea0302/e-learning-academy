8ee500c63fc21a9cbc7649ece917c0c75ae46fd8
AhmedRabea0302 <arabea169@gmail.com>
2023-08-02 14:14:42 +0300

    Added Excerpt field to events cutom post type

--- app/public/wp-content/mu-plugins/academy-post-types.php
+++ app/public/wp-content/mu-plugins/academy-post-types.php
@@ -3,6 +3,10 @@
     add_action('init', 'academy_post_types');
     function academy_post_types() {
         register_post_type('event', array(
+            'show_in_rest' => true,
+            'supports' => array('title', 'editor', 'excerpt'),
+            'rewrite' => array('slug' => 'events'),
+            'has_archive' => true,
             'public' => true,
             'menu_icon' => 'dashicons-calendar-alt',
             'labels' => array(
