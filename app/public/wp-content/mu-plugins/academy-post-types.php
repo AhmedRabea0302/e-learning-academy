719dba457b4dadefabf453a27ebb94144fd8ef2d
AhmedRabea0302 <arabea169@gmail.com>
2023-08-02 20:09:53 +0300

    Added Programs

--- app/public/wp-content/mu-plugins/academy-post-types.php
+++ app/public/wp-content/mu-plugins/academy-post-types.php
@@ -2,6 +2,7 @@
 
     add_action('init', 'academy_post_types');
     function academy_post_types() {
+        // Event Post Type
         register_post_type('event', array(
             'show_in_rest' => true,
             'supports' => array('title', 'editor', 'excerpt'),
@@ -15,6 +16,38 @@
                 'edit_item' => 'Edit Event',
                 'all_items' => 'All Events',
                 'singular_name' => 'Event'
+            ),
+        ));
+
+        // Program Post Type
+        register_post_type('program', array(
+            'show_in_rest' => true,
+            'supports' => array('title', 'editor'),
+            'rewrite' => array('slug' => 'programs'),
+            'has_archive' => true,
+            'public' => true,
+            'menu_icon' => 'dashicons-awards',
+            'labels' => array(
+                'name' => 'Programs',
+                'add_new_item' => 'Add New Program',
+                'edit_item' => 'Edit Program',
+                'all_items' => 'All Programs',
+                'singular_name' => 'Program'
+            ),
+        ));
+
+         // Professor Post Type
+         register_post_type('professor', array(
+            'show_in_rest' => true,
+            'supports' => array('title', 'editor'),
+            'public' => true,
+            'menu_icon' => 'dashicons-welcome-learn-more',
+            'labels' => array(
+                'name' => 'Professors',
+                'add_new_item' => 'Add New Professor',
+                'edit_item' => 'Edit Professor',
+                'all_items' => 'All Professors',
+                'singular_name' => 'Professor'
             ),
         ));
     }
